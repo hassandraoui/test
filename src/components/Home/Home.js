7d95ef0808fcd39f656e78423ac52d39952776b8
hassandraoui <114435542+hassandraoui@users.noreply.github.com>
2022-10-01 15:29:32 +0200

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -22,7 +22,7 @@ function Home() {
 
               <h1 className="heading-name">
                 I'M
-                <strong className="main-name"> Eng. DRAOUI Hassan</strong>
+                <strong className="main-name"> Eng. DRAOUI Hassan ID/IDOSR</strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
